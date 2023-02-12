7fa739345342eb02aad90e0d6c37541294903ceb
KurohChen <465344912@qq.com>
2023-02-12 12:50:55 +0800

    "displays the Login information"

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/LoginTicketInterceptor.java
@@ -0,0 +1,60 @@
+package com.nowcoder.community.controller.interceptor;
+
+import com.nowcoder.community.entity.LoginTicket;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CookieUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class LoginTicketInterceptor implements HandlerInterceptor {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    //请求前
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //从cookie中获取凭证
+        String ticket = CookieUtil.getValue(request,"ticket");
+        if(ticket != null){
+            LoginTicket loginTicket = userService.findLoginTicket(ticket);
+            if(loginTicket != null && loginTicket.getStatus() == 0 && loginTicket.getExpired().after(new Date())){
+                System.out.println(ticket);
+                System.out.println(loginTicket.getStatus());
+                System.out.println(loginTicket.getExpired().toString());
+                // 根据凭证查询用户
+                User user = userService.findUserById(loginTicket.getUserId());
+                // 本次请求持有用户
+                // 多线程情况：所以考虑线程隔离
+                hostHolder.setUser(user);
+            }
+        }
+        return true;
+    }
+
+    //模板之前调用
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if(user!=null && modelAndView!=null){
+            modelAndView.addObject("loginUser",user);
+        }
+    }
+
+    //整个请求结束后,清除
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        hostHolder.clear();
+    }
+}
