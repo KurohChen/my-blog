b7d4f390f5f164e261381043bb53ab5cf0f23600
KurohChen <465344912@qq.com>
2023-02-18 14:48:37 +0800

    "add transaction"

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,13 +1,26 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 // 表示是一个业务组件
 @Service
@@ -17,6 +30,12 @@ public class AlphaService {
     // controller调用service，service调用Dao，所以在这里运用依赖注入
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
 
     // 对比
     public AlphaService(){
@@ -37,6 +56,73 @@ public class AlphaService {
 
     public String find(){
         return alphaDao.select();
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED,propagation = Propagation.REQUIRED)
+    public Object save1(){
+        // 测试事务:读取已提交的数据
+        // 传播机制：
+        // Required：支持当前事务（调用者、外部事务），如果不存在，创建新事物
+        // Required_new：创建一个新的事务并且暂停外部事务
+        // nested：如果当前存在外部事务，则嵌套在该事务中执行（独立的提交和回滚），否则就会和required一致
+        //新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        //新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("hello");
+        post.setContent("新人报到");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        //加错
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+    public Object save2(){
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                //新增用户
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+                user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                //新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("你好");
+                post.setContent("新人报到");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                //加错
+                Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
     }
 
 
