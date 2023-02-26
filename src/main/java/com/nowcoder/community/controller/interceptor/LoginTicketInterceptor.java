40c48673604f3dfb7b52aa05941b800b9530c9f2
KurohChen <465344912@qq.com>
2023-02-26 15:55:12 +0800

    "Add authority"

--- src/main/java/com/nowcoder/community/controller/interceptor/LoginTicketInterceptor.java
+++ src/main/java/com/nowcoder/community/controller/interceptor/LoginTicketInterceptor.java
@@ -6,6 +6,11 @@ import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CookieUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -38,6 +43,12 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
                 // 本次请求持有用户
                 // 多线程情况：所以考虑线程隔离
                 hostHolder.setUser(user);
+                //构建用户认证的结果并存入SecurityContext,以便于Security进行授权
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        user,user.getPassword(),userService.getAuthorities(user.getId())
+                );
+
+                SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
             }
         }
         return true;
@@ -56,5 +67,6 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         hostHolder.clear();
+        SecurityContextHolder.clearContext();
     }
 }
